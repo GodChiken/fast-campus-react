3b83898b7824973c85a8a251ca9f92ef8db3ab5f
Bo-Hun Kim <godchiken@naver.com>
2019-01-27 17:03:33 +0900

    코드 정리

--- src/style/classnames/classNamesExample.js
+++ src/style/classnames/classNamesExample.js
@@ -12,3 +12,24 @@ class ClassNamesExample extends Component {
 }
 
 export default ClassNamesExample;
+
+
+// // classnames 미적용시
+// className={['Button', size].join(' ')}
+// className={`Button ${size}`}
+//
+// // classnames 사용법
+//
+// classNames('foo', 'bar'); // => 'foo bar'
+// classNames('foo', { bar: true }); // => 'foo bar'
+// classNames({ 'foo-bar': true }); // => 'foo-bar'
+// classNames({ 'foo-bar': false }); // => ''
+// classNames({ foo: true }, { bar: true }); // => 'foo bar'
+// classNames({ foo: true, bar: true }); // => 'foo bar'
+// classNames(['foo', 'bar']); // => 'foo bar'
+//
+// // 동시에 여러개의 타입으로 받아올 수 도 있습니다.
+// classNames('foo', { bar: true, duck: false }, 'baz', { quux: true }); // => 'foo bar baz quux'
+//
+// // false, null, 0, undefined 는 무시됩니다.
+// classNames(null, false, 'bar', undefined, 0, 1, { baz: null }, ''); // => 'bar 1'
