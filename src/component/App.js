d889f4250a2c7e0d2a5f5c4f3236128119d24632
GodChiken <godchiken@naver.com>
2019-11-05 15:35:39 +0900

    실습 완료분

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import Wrapper from './Wrapper'
-import Hello from './Hello'
+import Counter from "./Counter";
 
 export default function App() {
     return (
-        <Wrapper>
-            <Hello name="react" color="red" isSpecial></Hello>
-            <Hello color="pink"></Hello>
-        </Wrapper>
+        <Counter/>
     );
 }
