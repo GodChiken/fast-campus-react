9ab9fd3b692f03d8cac9b8320854f92664d63a36
GodChiken <godchiken@naver.com>
2019-11-07 13:42:05 +0900

    16. useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기 - 실습 완료분

--- src/component/UserList.js
+++ src/component/UserList.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function User({ user, onRemove, onToggle}) {
+    useEffect(() =>{
+        console.log("컴포넌트 마운트!");
+        console.log(user);
+        return () => {
+            console.log("컴포넌트가 화면에서 사라짐");
+            console.log(user);
+        }
+    },[user]);
+    // useEffect(() =>{
+    //     console.log(user);
+    // });
     return (
         <div>
             <b
