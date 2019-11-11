4c36ed6a51b7882b462835db657df7f367dbc2d9
GodChiken <godchiken@naver.com>
2019-11-11 10:46:35 +0900

    20. useReducer 를 사용하여 상태 업데이트 로직 분리하기 - useState Counter 구현분

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import {render} from 'react-dom'
 import './index.css';
 import App from "./component/App";
+import Counter from "./component/Counter";
 
-render(<App/>,document.getElementById('root'));
+render(<Counter/>,document.getElementById('root'));
+//render(<App/>,document.getElementById('root'));
