39a2ead13827be32c42ba1d1e4f92f4d39331d1a
GodChiken <godchiken@naver.com>
2019-11-21 15:35:51 +0900

    24. 클래스형 컴포넌트 - 생성자 binding 해결법

--- src/component/class/Counter.js
+++ src/component/class/Counter.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
 
 export default class Counter extends Component{
+
+    constructor(props) {
+        super(props);
+        this.handleIncrease = this.handleIncrease.bind(this);
+        this.handleDecrease = this.handleDecrease.bind(this);
+    }
+
     handleIncrease() {
         console.log('increase');
         console.log(this);                 // 컴포넌트 인스턴스를 가르키지 않음.
