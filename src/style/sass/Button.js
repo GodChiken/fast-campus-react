9298d9d2701215c58a7a00a57335315fa53ca70a
godchiken <godchiken@naver.com>
2019-12-22 17:11:15 +0900

    28. SASS - onEvent rest 문법으로 전달하기 실습분

--- src/style/sass/Button.js
+++ src/style/sass/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 import classNames from 'classnames';
 import './Button.scss';
 
-export default function Button({ children, size, color, outline, fullWidth }) {
+export default function Button({ children, size, color, outline, fullWidth, ...rest }) {
     return (
-        <button className={classNames('Button', size, color, { outline, fullWidth })}>
+        <button className={classNames('Button', size, color, { outline, fullWidth })} {...rest} >
             {children}
         </button>
     );
