fa9948ed479bc0394b5a6eaea9679d08f91b8350
김보훈 <funnic2@gmail.com>
2019-12-27 17:38:09 +0900

    30. styledComponent - 각 컴포넌트 혼용 실습

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import './css/styles.css';
 import App from './style/styledcomponent/App';
 
-//ReactDOM.render(<Box />, document.getElementById('root'));
 ReactDOM.render(<App/>,document.getElementById('root'));
