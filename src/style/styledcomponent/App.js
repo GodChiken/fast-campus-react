7823d10c5408381445e7af799d01e61082c58b5b
김보훈 <funnic2@gmail.com>
2019-12-27 17:28:24 +0900

    30. styledComponent - props 전달

--- src/style/styledcomponent/App.js
+++ src/style/styledcomponent/App.js
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 const Circle = styled.div`
   width: 5rem;
   height: 5rem;
-  background: black;
+  background: ${props => props.color || 'black'};
   border-radius: 50%;
 `;
 
 export default function App() {
-    return <Circle />;
+    return <Circle color="blue"/>;
 }
